dbf2b190407575bbd07bf9f8f3fd3f60d21a8607
Katherine Wren <kwren@outlook.com>
2019-05-02 14:40:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+class School
+  
+attr_reader :name, :roster, :i
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, student_grade)
+    if @roster.include?(student_grade)
+    @i = roster[student_grade].length
+    roster[student_grade][@i] << student_name
+    else
+    roster[student_grade] = []
+    roster[student_grade] << student_name
+    end
+  end
+  
+  def grade
+    
+  end
+  
+  
+  def sort
+    @roster
+  end
+  
+end
+
+puts "Mischief Managed"
